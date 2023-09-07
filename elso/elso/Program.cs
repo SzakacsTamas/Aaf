716aca15748ec949bee674a28adff40217949816
Szakács Tamás <szakacst21@ADY-NAGYATAD.SULINET.HU>
2023-09-07 08:44:07 +0200

    harmadik

--- elso/elso/Program.cs
+++ elso/elso/Program.cs
@@ -14,8 +14,47 @@ namespace elso
             Console.WriteLine("Helló Világ!"[4]);
             double szam = 8.5;
             Console.WriteLine(szam);
-            char a = 'k';
-            Console.WriteLine(a);
+            char s = 'k';
+            Console.WriteLine(s);
+            Console.WriteLine((char)(s+2));
+            int i = 0;
+            ++i;
+            int a;
+            a =4;
+
+            Console.WriteLine(i);
+            if (a > 2)
+            {
+                Console.WriteLine("Nagyobb");
+            }
+            else if(a % 2 ==0)
+            {
+                Console.WriteLine("Páros");
+            }
+            else
+            {
+                Console.WriteLine("Egyenlő");
+            }
+
+            switch (a)
+            {
+                case 1:
+                    Console.WriteLine("egy");
+                    break;
+                case 2:
+                    Console.WriteLine("kettő");
+                    break;
+                case 3:
+                    Console.WriteLine("három");
+                    break;
+                case 4:
+                    Console.WriteLine("négy");
+                    break;
+                default:
+                    Console.WriteLine("valami más");
+                    break;
+            }
+            
         }
     }
 }
