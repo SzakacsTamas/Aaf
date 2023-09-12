325d75fb0de3fd09833265090b5e423fbd4352a3
Szakács Tamás <szakacst21@ADY-NAGYATAD.SULINET.HU>
2023-09-12 14:00:46 +0200

    otodik

---
+++ Bekeres/Bekeres/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bekeres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   /*
+            Console.WriteLine("Kérek egy szöveget: ");
+            string szoveg=Console.ReadLine();
+            Console.WriteLine("A bekért szöveg: " + szoveg);
+
+            //üres sor végjelig
+            szoveg = " ";
+            while(szoveg!="")
+            {
+                Console.Write("Kérek egy szót: ");
+                szoveg = Console.ReadLine();
+            }
+            //HÁZIIIIIIIIIIIIIII szavak bekérése, üres végjlelig, 
+            //összegyűjtve egy váltózóban, valahogy elválasztva
+
+            Console.Write("Kérek egy nem egész számot");
+            double szam = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("A szám négyzete"+(szam *szam+1));
+            Kérj be egy egész számot és annyiszor fusson le a szöveg bekérés és pl 5, 5x kérjeb be efy szót
+
+            */
+            Console.Write("Kérek egy egész számot: ");
+            int szam = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < szam; i++)
+            {
+                Console.Write("Kérem a(z) " + (i + 1) + ". szöveget: ");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ hazi/hazi/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hazi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i <= 95; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
