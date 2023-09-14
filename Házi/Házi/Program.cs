588dcc149455ee69d3633d862aca5721edb68210
SzakacsTamas <98027548+SzakacsTamas@users.noreply.github.com>
2023-09-14 18:30:00 +0200

    Add files via upload

---
+++ Házi/Házi/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Kérem, adja meg az első számot: ");
+int szam1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Kérem, adja meg a második számot: ");
+int szam2 = Convert.ToInt32(Console.ReadLine());
+
+int osszeg = 0;
+
+// Két szám közötti számok összegzése
+for (int i = szam1; i <= szam2; i++)
+{
+    osszeg += i;
+}
+
+int negyzet = osszeg * osszeg;
+
+Console.WriteLine("A(z) "+szam1 + " és "+ szam2+" közötti számok összegének négyzete: "+ negyzet);
