dd6c52fc6fdf334cf06a198e6c68eb7f93ac14ba
Szakács Tamás <szakacst21@ADY-NAGYATAD.SULINET.HU>
2023-09-15 11:29:33 +0200

    UtolsoNap

---
+++ Gyakorlas/Gyakorlas/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyakorlas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int szam = 0;
+            while (szam < 99999)
+            {
+                Console.Write("Kérek egy számot: ");
+
+                szam = Convert.ToInt32(Console.ReadLine());
+                if (szam < 10)
+                {
+                    Console.WriteLine("A szám egy jegyű");
+                }
+                
+
+            }
+        }
+    }
+}
---
+++ gyakorlas0915/gyakorlas0915/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Console.Write("Kérjen be egy egész számot: ");
+int szam = Convert.ToInt32(Console.ReadLine());
+for (int i=0; i<szam; i++)
+{
+    Console.WriteLine(i*10);
+}
+*/
+/*
+Console.Write("Kérjen be egy egész számot: ");
+int a = Convert.ToInt32(Console.ReadLine());
+for (int i=1; i<=a; i++)
+{
+    for (int k=1;k<a;k++)
+    {
+        Console.Write(i*k+"\t");
+    }
+    Console.WriteLine();
+}
+*/
+
+for (int i = 0; i < 999; i++)
+{
+    Console.WriteLine(i + i);
+}
