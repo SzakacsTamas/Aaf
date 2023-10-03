ce9e85f176ea7b7332d1ce5576491462c4d65954
Szakács Tamás <szakacst21@ADY-NAGYATAD.SULINET.HU>
2023-10-03 09:32:42 +0200

    kedd

---
+++ mertEgyseg/mertEgyseg/Program.cs
@@ -0,0 +1,29 @@
+﻿bool jo = false;
+
+while (!jo)
+{
+    jo = true;
+    Console.Write("Mit Váltsak át?: ");
+    string be = Console.ReadLine();
+
+    string[] darab = be.Split(' ');
+    if (darab.Length != 2)
+    {
+        
+        jo = false;
+        continue;
+    }
+    double szam = 0;
+    try
+    {
+        szam = Convert.ToDouble(darab[0]);
+    }
+    catch (Exception)
+    {
+
+        Console.WriteLine("Nem jó formátum! Legyen ilyen: 10,2 kg");
+        
+    }
+    Console.WriteLine(szam);
+
+}
