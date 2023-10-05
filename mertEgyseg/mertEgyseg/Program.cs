651b590376bb4ddd8c30c693582bfab24199784a
Szakács Tamás <szakacst21@ADY-NAGYATAD.SULINET.HU>
2023-10-05 08:44:14 +0200

    egyseg

--- mertEgyseg/mertEgyseg/Program.cs
+++ mertEgyseg/mertEgyseg/Program.cs
@@ -1,4 +1,11 @@
-﻿bool jo = false;
+﻿//string[] cars = {"Volvo","BMW","Ford","Mazda"};
+
+string[] hosszusag = { "mm","cm","dm","m","km"};
+string[] tomeg = { "g", "dkg", "kg", "t", "kt","Mt" };
+string[] urmertek = { "ml", "cl", "dl", "l", "hl" };
+string[] terfogat = { "mm3", "cm3", "dm3", "m3", "km3" };
+
+bool jo = false;
 
 while (!jo)
 {
@@ -29,4 +36,20 @@ while (!jo)
     }
     Console.WriteLine(szam);
 
+    if (hosszusag.Contains(darab[1]))
+    {
+    }
+    else
+    {
+        Console.WriteLine("Nem jó mértékegység");
+        jo = false;
+        continue;
+    }
 }
+Console.WriteLine("Mire szeretnéd átváltani?: ");
+for (int i = 0; i < hosszusag.Length; i++)
+{
+    Console.WriteLine(hosszusag[i]);
+}
+Console.Write("Válassz: ");
+string valasz=Console.ReadLine();
