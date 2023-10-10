957d515196648c0505406d73b3de2c72ceb7631e
Szakács Tamás <szakacst21@ADY-NAGYATAD.SULINET.HU>
2023-10-10 14:03:35 +0200

    tombok

---
+++ Tombok/Tombok/Program.cs
@@ -0,0 +1,27 @@
+﻿void kiir(int[] tomb)
+{
+    foreach (int i in tomb)
+    {
+        Console.WriteLine("{0}.szám", i);
+    }
+}
+
+int[] szamok = new int[100];
+
+for (int i = 0; i < szamok.Length; i++)
+{
+    szamok[i] = i+1;
+
+}
+foreach (int i in szamok)
+{
+    Console.WriteLine("{0}.szám",i);
+}
+
+
+for (int i = 0;i < szamok.Length; i++)
+{
+    szamok[i] *= 2;
+}
+
+kiir(szamok);
