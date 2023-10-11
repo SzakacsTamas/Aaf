c0126d7664f0a03188086f9b09152ed4c54af947
Szakács Tamás <szakacst21@ADY-NAGYATAD.SULINET.HU>
2023-10-11 12:34:10 +0200

    tombok2

--- Tombok/Tombok/Program.cs
+++ Tombok/Tombok/Program.cs
@@ -1,4 +1,5 @@
-﻿void kiir(int[] tomb)
+﻿/*
+void kiir(int[] tomb)
 {
     foreach (int i in tomb)
     {
@@ -25,3 +26,45 @@ for (int i = 0;i < szamok.Length; i++)
 }
 
 kiir(szamok);
+*/
+
+using Microsoft.VisualBasic;
+
+Console.Write("Hány sor legyen: ");
+int sor=Convert.ToInt32(Console.ReadLine());
+Console.Write("Hány oszlop legyen: ");
+int oszlop = Convert.ToInt32(Console.ReadLine());
+
+string[,] tabla =new string[sor,oszlop];
+
+for (int i = 0; i < tabla.GetLength(0); i++)
+{
+    for (int k = 0;k < tabla.GetLength(1); k++)
+    {
+        tabla[i, k] = "";
+        for (int m = 0;m < i+1; m++)
+        {
+            tabla[i, k] = tabla[i, k] +"a";
+        }
+        for (int m = 0; m < k + 1; m++)
+        {
+            tabla[i, k] = tabla[i, k] + "b";
+        }
+
+
+    }
+}
+
+foreach(string szo in tabla)
+{
+    Console.WriteLine(szo);
+}
+
+for (int i = 0;i < tabla.GetLength(0); i++)
+{
+    for (int k = 0; k < tabla.GetLength(1);k++)
+    {
+        Console.Write($"{tabla[i, k]}\t");
+    }
+    Console.WriteLine();
+}
