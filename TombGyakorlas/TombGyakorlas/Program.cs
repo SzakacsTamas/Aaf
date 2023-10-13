84c6b07b7668ba0b3f67629fadf79167d8dc35a0
Szakács Tamás <szakacst21@ADY-NAGYATAD.SULINET.HU>
2023-10-13 11:29:22 +0200

    Tombokk

--- TombGyakorlas/TombGyakorlas/Program.cs
+++ TombGyakorlas/TombGyakorlas/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.Write("Kérj be egy számot: ");
+﻿/*
+using System.ComponentModel.DataAnnotations;
+
+Console.Write("Kérj be egy számot: ");
 int sorSzam = Convert.ToInt32(Console.ReadLine());
 
 string[] szavak=new string[sorSzam];
@@ -12,8 +15,47 @@ for (int i = 0; i < sorSzam; i++)
 string max = "";
 for (int i = 0;i < szavak.Length; i++)
 {
-    if (szavak[i])
+    if (szavak[i].Length> max.Length)
+    {
+        max = szavak[i];
+    }
 }
+
+Console.WriteLine(max);
+
+//index kereses
+
+int maxIndex = 0;
+for (int i = 0; i< szavak.Length; i++)
+{
+    if (szavak[i]== max)
+    {
+        maxIndex = i;
+    }
+}
+
+max = "";
+maxIndex= 0;
+for (int i = 0; i < szavak.Length; i++)
+{
+    if (szavak[i].Length >max.Length) 
+    { 
+    max = szavak[i];
+    maxIndex = i;
+    }
+}
+
+//max kereses 3
+
+maxIndex = 0;
+for (int i = 0;i < szavak.Length; i++)
+{
+    if (szavak[i].Length> szavak[maxIndex].Length) {
+    maxIndex=i;
+    }
+}
+
+
 
 
 
@@ -21,3 +63,48 @@ for (int i = 0;i < szavak.Length; i++)
 
 
 //Console.WriteLine(hossz);
+*/
+
+using System.Net.Http.Headers;
+
+string[] szavak=new string[] {"alma","körte","narancs","mandula","ananász","banán"};
+
+string[] rovid = new string[szavak.Length];
+string[] hosszu = new string[szavak.Length];
+
+int rovidIndex = 0; 
+int hosszuIndex=0;
+
+for (int i = 0; i < szavak.Length; i++)
+{
+    if (szavak[i].Length <= 5)
+    {
+        rovid[rovidIndex] = szavak[i];
+        rovidIndex++;
+    }
+    else
+    {
+        hosszu[rovidIndex++] = szavak[i];
+    }
+}
+
+foreach(string s in rovid)
+{
+    Console.WriteLine(s);
+}
+foreach(string e in hosszu)
+{
+    Console.WriteLine(e);
+}
+
+
+
+
+
+
+
+
+
+
+
+
