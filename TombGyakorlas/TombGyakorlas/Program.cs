ae2bc722246ace42de2eebb9404cb24a96d46c86
Szakács Tamás <szakacst21@ADY-NAGYATAD.SULINET.HU>
2023-10-13 08:45:04 +0200

    TombokFeladat

---
+++ TombGyakorlas/TombGyakorlas/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Kérj be egy számot: ");
+int sorSzam = Convert.ToInt32(Console.ReadLine());
+
+string[] szavak=new string[sorSzam];
+
+for (int i = 0; i < sorSzam; i++)
+{
+    Console.Write("Kérj be egy szót: ");
+    szavak[i]= Console.ReadLine();
+}
+
+string max = "";
+for (int i = 0;i < szavak.Length; i++)
+{
+    if (szavak[i])
+}
+
+
+
+// Console.WriteLine(szavak.OrderByDescending(s => s.Length).First());
+
+
+//Console.WriteLine(hossz);
