92a083b084426a2ebd5d1f20a4a1cc46a6e21c4c
Szakács Tamás <szakacst21@ADY-NAGYATAD.SULINET.HU>
2023-10-20 11:29:12 +0200

    asdds

---
+++ Gyakorlas_2_1019/Gyakorlas_2_1019/Program.cs
@@ -0,0 +1,47 @@
+﻿//FELADAT1: Készíteni egy függvényt ami visszaad egy 10 ezer tagú, 4-jegyű pozitív számokat
+//tarlamazó tömböt 
+//FELADAT2: Megkeresni az olyan pontosan 3 tagú szakaszokat ami egymástól maximum 10 távolságra vannak
+
+int[] tombKeszit(int darab, int legkisebb, int legnagyobb)
+{
+    int[] tomb = new int[darab];
+    for (int i = 0; i < tomb.Length; i++)
+    {
+        Random rand = new Random();
+        tomb[i] = rand.Next(legkisebb, legnagyobb + 1);
+    }
+    return tomb;
+}
+int[] szamok = tombKeszit(10000, 1000, 9999);
+
+void szakasz2(int[] szamok)
+{
+    int kezd = 0;
+    int veg = 0;
+    int maxKezd = 0;
+    int maxVeg = 0;
+    for (int i = 1; i < szamok.Length; i++)
+    {
+        if ( <= szamok[i]szamok[i - 1])
+        {
+
+        }
+        else
+        {
+            veg = i - 1;
+            if (veg - kezd + 1 > maxVeg - maxKezd + 1)
+            {
+                maxVeg = veg;
+                maxKezd = kezd;
+            }
+            kezd = i;
+
+        }
+    }
+    for (int i = maxKezd; i < maxVeg + 1; i++)
+    {
+        Console.Write("{0}, ", szamok[i]);
+    }
+    Console.WriteLine();
+}
+szakasz2(szamok);
---
+++ gyakorlas1020/gyakorlas1020/Program.cs
@@ -0,0 +1,23 @@
+﻿string szoveg = "Helló Bence!";
+for (int i = 0; i < szoveg.Length; i++)
+{
+    Console.WriteLine(szoveg[i]);
+}
+
+foreach(char s in szoveg)
+{
+    Console.WriteLine(s);
+}
+for (int i = 0;i < szoveg.Length; i++)
+{
+    Console.WriteLine(szoveg.Substring(i,1));
+}
+
+Console.WriteLine(szoveg.ToUpper());
+
+string[] darab=szoveg.Split(' ');
+Console.WriteLine(darab[0] +" " + darab[1].ToUpper());
+
+Console.WriteLine(szoveg.Substring(0,szoveg.IndexOf(" ")) +szoveg.Substring(szoveg.IndexOf("")+1;
+
+//HÁZI:::: BEKÉRNI EGY TÖBB SZÓBÓL ÁLLÓ SZÖVEGET MINDEN SZÓ KEZDŐBETŰJE LEGYEN NAGY A TÖBBI KICSI
