89ae1e25b863e5359d90440541b88417a864277a
Szakács Tamás <szakacst21@ADY-NAGYATAD.SULINET.HU>
2023-10-24 14:01:06 +0200

    gyakssz

--- gyakorlas1024/gyakorlas1024/Program.cs
+++ gyakorlas1024/gyakorlas1024/Program.cs
@@ -3,20 +3,67 @@
 
 //bekérünk egy szót végig nézzük betünként ciklussal minden betűt ellenőrzünk hogy benne van e a hasznalható betük stringjében, ha benne van, ha nincs akkor kiirjuk hogy ez nem szó
 
-string betuk = "QWERTZUIOPŐÚASDFGHJKLÉÁŰÍYXCVBNMÖÜÓ";
+string betuk = "QWERTZUIOPŐÚASDFGHJKLÉÁŰÍYXCVBNMÖÜÓqwertzuiopőúöüóasdfghjkléáűíyxcvbnm";
 string szo = "";
+bool joE=true;
 while (szo == "")
 {
     Console.WriteLine("kérek egy szót: ");
     szo= Console.ReadLine();
     for(int i = 0; i < szo.Length; i++)
     {
+        if (betuk.IndexOf(betuk[i]) < 0)
+        {
+            Console.WriteLine("Nem szó!");
+            joE = false;
+            break;
+
+        }
+        /*
         for(int j = 0; j < szo.Length; j++)
         {
             if (szo[i].ToString().ToLower() != betuk[j].ToString())
             {
-                if (szo[i] == )
+                if (szo[i] == betuk[j])
+                {
+
+                }
             }
         }
+        */
+    }
+
+    if(joE)
+    {
+        Console.WriteLine("{0} jó szó!", szo);
+    }
+    string ujSzo = "";
+    for(int i = szo.Length-1; i>=0; i--)
+    {
+        ujSzo += szo[i];    
+    }
+    Console.WriteLine(ujSzo);
+
+
+    ujSzo = "";
+    for(int i = 0; i < szo.Length; i++)
+    {
+        ujSzo+= szo[szo.Length-i];
+    }
+    Console.WriteLine(ujSzo);
+
+    ujSzo = "";
+    for (int i = 0; i < szo.Length; i++)
+    {
+        ujSzo = szo[i]+ujSzo;
+    }
+    Console.WriteLine(ujSzo);
+
+    ujSzo = "";
+    for (int i = 0; i < szo.Length; i+=2)
+    {
+        ujSzo+=szo.Substring(szo.Length-1-1)
     }
 }
+
+
