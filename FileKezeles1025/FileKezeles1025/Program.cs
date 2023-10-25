e25242f3004a63e5b8c14895e13ccd5fe2832f81
Szakács Tamás <szakacst21@ADY-NAGYATAD.SULINET.HU>
2023-10-25 12:32:41 +0200

    igen

---
+++ FileKezeles1025/FileKezeles1025/Program.cs
@@ -0,0 +1,37 @@
+﻿/* KÉrjünk be szavakat és ezek szavak legyenek!(csak betű legyen benne)
+ * Ezeket szavanként írjuk bele egy fileba
+*/
+
+string betuk = "qwertzuiopőúöüóasdfghjkléáűíyxcvbnm-";
+string beSzo = "a";
+string gyujto = "";
+while (beSzo != "")
+{
+    bool joSzo = true;
+
+    Console.WriteLine("Kérek egy szót");
+    beSzo = Console.ReadLine();
+
+    for (int i = 0; i < beSzo.Length; i++)
+    {
+        if (betuk.IndexOf(beSzo[i]) == -1)
+        {
+            joSzo = false;
+            Console.WriteLine("Ez nem jó szó!");
+            break;
+        }
+    }
+    if (joSzo)
+    {
+        gyujto+= beSzo+" ";
+        
+    }
+}
+string[] darabok=gyujto.Split(' ');
+StreamWriter ir = new StreamWriter("szavak.txt");
+for (int i = 0;i < darabok.Length;i++)
+{
+    ir.WriteLine(darabok[i]);
+}
+ir.Close();
+File.WriteAllLines("szavak2.txt", darabok);
