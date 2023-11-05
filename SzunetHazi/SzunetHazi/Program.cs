2425c637ba7d9437547024fd3fe622df3b4cd482
SzakacsTamas <98027548+SzakacsTamas@users.noreply.github.com>
2023-11-05 11:43:11 +0100

    Add files via upload

---
+++ SzunetHazi/SzunetHazi/Program.cs
@@ -0,0 +1,25 @@
+﻿static string szogeneralas(int hanybetus)
+{
+    string betuk = "qwertzuiopőúasdfghjkléáűíyxcvbnmöüó";
+
+    string szo = "";
+    var rand = new Random();
+    for (int i = 0; i < hanybetus; i++)
+    {
+        szo += betuk[rand.Next(betuk.Length)];
+    }
+
+    return szo;
+    
+}
+
+var rand = new Random();
+string[] szavak = new string[10000];
+for (int i = 0; i < 10000; i++)
+{
+    szavak[i] = szogeneralas(rand.Next(4, 7));
+}
+
+File.WriteAllLines("szavak.txt", szavak);
+
+string[] szavak2 = File.ReadAllLines("szavak.txt");
