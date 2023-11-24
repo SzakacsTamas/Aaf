8fab325bb106d72e94d3022b06564018dcce3751
Szakács Tamás <szakacst21@ADY-NAGYATAD.SULINET.HU>
2023-11-24 11:30:27 +0100

    ghgjz

--- szalloda/szalloda/Program.cs
+++ szalloda/szalloda/Program.cs
@@ -7,13 +7,25 @@ static int fizetendo(int erkezes, int tavozas, int szemelydb, bool etkezes)
     else szobaAr = 8000;
 
     int ejDb = tavozas - erkezes;
-    if (szemelydb>2 ==)
+    if (szemelydb>2)
     {
         szobaAr += 2000;
     }
     if (etkezes)
         szobaAr += szemelydb + 1100;
     return szobaAr*ejDb;
+}
+
+static int melyikHonap(int napdb, List<int> holKezdodik)
+{
+    for (int i = 0;i < holKezdodik.Count; i++)
+    {
+        if(napdb < holKezdodik[i])
+        {
+            return i;
+        }
+    }
+    return 12;
 }
 
 StreamReader olvas = new StreamReader("pitypang.txt");
@@ -59,8 +71,44 @@ for (int i = 0; i < sorszamok.Count; i++)
 Console.WriteLine("2 feladat");
 Console.WriteLine("{0} ({1}) - {2}", vendeg[maxId], erkezesNapja[maxId],maxEj);
 
+int osszeg = 0;
 StreamWriter ir = new StreamWriter("bevetel.txt");
 for(int i = 0;i < sorszamok.Count; i++)
 {
-    ir.WriteLine("{0}:{1}", sorszamok[i], fizetendo(erkezesNapja[i], tavozasNapja[i], vendegSzam[i], etkezes[i]));
+    int fiz= fizetendo(erkezesNapja[i], tavozasNapja[i], vendegSzam[i], etkezes[i]);
+    ir.WriteLine("{0}:{1}", sorszamok[i], fiz);
+    osszeg += fiz;
 }
+ir.Close();
+Console.WriteLine("Az éves bevétel {0:n0} Ft", osszeg);
+
+olvas = new StreamReader("honapok.txt");
+List<string> honapok = new List<string>();
+List<int> napSzam = new List<int>();
+List<int> holKezdodik = new List<int>();
+
+int mutato = 0;
+while (olvas.EndOfStream)
+{
+    string sor = olvas.ReadLine();
+    if (mutato == 0)
+    {
+        honapok.Add(sor);
+    }
+    else if (mutato == 1)
+    {
+        napSzam.Add(Convert.ToInt32(sor));
+    }
+    else
+    {
+        holKezdodik.Add(Convert.ToInt32(sor));
+        mutato = -1;
+    }
+}
+olvas.Close();
+Dictionary<int,int> vendegejszaka = new Dictionary<int,int>();
+for(int i =0;  i < sorszamok.Count; i++)
+    {
+        Console.WriteLine(erkezesNapja[i] + "-" + tavozasNapja[i]);
+    }
+Console.WriteLine(melyikHonap(1, holKezdodik));
