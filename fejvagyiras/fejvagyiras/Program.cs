4833938ee890049a8adacdc292799d5265fe0931
Szakács Tamás <szakacst21@ADY-NAGYATAD.SULINET.HU>
2024-01-26 08:45:32 +0100

    tjgje

---
+++ fejvagyiras/fejvagyiras/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using fejvagyiras;
+
+List<adatok> dobasok=new List<adatok>();
+StreamReader olvas = new StreamReader("kiserlet.txt");
+while (!olvas.EndOfStream)
+{
+    string sor = olvas.ReadLine();
+    if (dobasok[dobasok.Count - 1].dobas == sor)
+    {
+        dobasok[dobasok.Count - 1].db++;
+    }
+}
+
+olvas.Close();
---
+++ fejvagyiras/fejvagyiras/adatok.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fejvagyiras
+{
+    internal class adatok
+    {
+        public string dobas;
+        public int db;
+        public adatok(string dobas)
+        {
+            this.dobas = dobas;
+            db = 1;
+        }
+    }
+}
